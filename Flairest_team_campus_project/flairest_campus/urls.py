3def4d398315468051b2cfbbbb23a2caf665f565
Smorodina <ha.muhumajlkax@gmail.com>
2024-04-01 18:12:50 +0500

    Исправлена кнопка вузы на странице университета

--- Flairest_team_campus_project/flairest_campus/urls.py
+++ Flairest_team_campus_project/flairest_campus/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('review_add_DEVELOPMENT', views.RevAdd.as_view()),
     path('', views.home_page),
     path('profile/', views.profile_page, name='profile'),
-    path('catalog/', views.UniCatalog1, name='catalog'),
+    path('catalog/', views.UniCatalog, name='catalog'),
     path('unicreate/', views.UniAdd.as_view(), name="add_uni"),
     path('catalog/uni/<int:uni_id>/', views.uni_detail, name='curr_uni'),
     path('speccreate/', views.SpecAdd.as_view(), name="add_spec"),
